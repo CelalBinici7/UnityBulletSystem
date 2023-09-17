a742e10bf34ed33a2b3356b5b2117e5d8c5f4176
Celal Binici <61542865+CelalBinici7@users.noreply.github.com>
2023-09-17 11:07:11 +0300

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyScript : MonoBehaviour
+{
+    public int TotalBullets;
+    public int MagazineCapacity;
+    public int RemainingBullets;
+    public string gun_name;
+    public TMP_Text TotalBulletsText;
+    public TMP_Text RemainingBulletsText;
+
+    void bulletstate(string state)
+    {
+        switch (state)
+        {
+            case "bullets":
+                if (TotalBullets <= MagazineCapacity)
+                {
+                    int pulse = RemainingBullets += TotalBullets;
+                    if (pulse > MagazineCapacity)
+                    {
+                        TotalBullets = pulse - MagazineCapacity;
+                        RemainingBullets = MagazineCapacity;
+                        PlayerPrefs.SetInt(gun_name + "_Bullet", TotalBullets);
+                    }
+                    else if (pulse < MagazineCapacity)
+                    {
+                        RemainingBullets = pulse;
+                        TotalBullets = 0;
+                        PlayerPrefs.SetInt(gun_name + "_Bullet", TotalBullets);
+                    }
+                    
+                }
+                else
+                {
+                    spendbullet = MagazineCapacity - RemainingBullets;
+                    TotalBullets -= spendbullet;
+                    RemainingBullets = MagazineCapacity;
+                    PlayerPrefs.SetInt(gun_name + "_Bullet", TotalBullets);
+                }
+                TotalBulletsText.text = TotalBullets.ToString();
+                RemainingBulletsText.text = RemainingBullets.ToString();
+                break;
+            case "nobullets":
+                if (TotalBullets <= MagazineCapacity)
+                {
+                    RemainingBullets = TotalBullets;
+                    TotalBullets = 0;
+                    PlayerPrefs.SetInt(gun_name + "_Bullet", TotalBullets);
+                }
+                else
+                {
+                    TotalBullets -= MagazineCapacity;
+                    RemainingBullets = MagazineCapacity;
+                    PlayerPrefs.SetInt(gun_name + "_Bullet", TotalBullets);
+                }
+
+                TotalBulletsText.text = TotalBullets.ToString();
+                RemainingBulletsText.text = RemainingBullets.ToString();
+                break;
+            case "print":
+                TotalBulletsText.text = TotalBullets.ToString();
+                RemainingBulletsText.text = RemainingBullets.ToString();
+                break;
+        }
+    }
+}
